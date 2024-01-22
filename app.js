730d6eac8a8dded65bdaa2ea7ce3dc8dd5ce84ab
PranavBhatia <bhatiapranav1996@gmail.com>
2024-01-22 16:10:45 -0500

    Sending response

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Node.js Website</title></head>");
+  res.write("<body><h1>Node.js Website Demo</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3001);
